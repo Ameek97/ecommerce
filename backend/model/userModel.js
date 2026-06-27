b455342bc18f317b2d4251bc734b43ed6665f202
Ameek <ameekajaz2@gmail.com>
2026-06-27 16:55:02 +0530

    make changes to user model

---
+++ backend/model/userModel.js
@@ -0,0 +1,65 @@
+import mongoose from "mongoose" 
+import validator from "validator"
+
+const DB = process.env.DATABASE.replace('<db_password>', process.env.PASSWORD);
+
+mongoose.connect(DB)
+.than(()=>{
+    console.log("connection to mongo database succesful");
+}).catch(err => {
+        console.error('Connection error:', err.message);
+    });
+ 
+const userSchema = new mongoose.Schema({
+
+   name:{
+    type:String, }, 
+
+   role:{
+    type:String,
+    enu:["user","admin"],
+    default:"user"
+   }, 
+
+   email:{
+    type:String,
+    unique: [true, "This email is already registered"],
+    validate: [validator.isEmail, "this email is not valid"],
+
+    },
+
+   password:{
+    type:String,
+    required:true,
+    select:false
+   },
+
+   passwordConfirm:{
+    type:string,
+
+    validate:{
+        validator: function(el){
+            return el== this.password
+        },
+        message:"the password does not match"
+             }    
+
+                     },
+   
+    passwordChangedAt: Date,
+    passwordResetToken: String,
+    passwordTokenExpire: Date,              
+
+
+    active:{
+        type:Boolean,
+        default:true,
+        select:false
+    } 
+
+});
+
+
+
+const User = mongosse.Model('User', userSchema);
+module.exports = User;    
